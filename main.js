7aea17e5587b2b202d564336a4e00531d986cbff
veekcs <veecks>
2022-05-18 08:57:56 -0300

    Interactions between sqrs and made both draggable

--- main.js
+++ main.js
@@ -14,8 +14,13 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 var friction = 2;
-var g = 0.5;
+var g = 1;
+var objects = [];
+var clickX = 0;
+var clickY = 0;
+var beingDragged = null;
 var sq;
+var sq2;
 var floor;
 var lastUpdate;
 var deltaTime = 0;
@@ -34,10 +39,13 @@ var Object2D = /** @class */ (function () {
         document.body.appendChild(this.obj);
         this.x = x;
         this.y = y;
+        this.prev_x = this.x;
+        this.prev_y = this.y;
         this.velX = 0;
         this.velY = 0;
         this.mass = 100;
-        this.bounce = 0.8;
+        this.bounce = 0.7;
+        this.components = [];
     }
     Object.defineProperty(Object2D.prototype, "size_x", {
         get: function () { return parseFloat(this.obj.style.width.slice(0, -2)); },
@@ -51,23 +59,79 @@ var Object2D = /** @class */ (function () {
     });
     Object.defineProperty(Object2D.prototype, "x", {
         get: function () { return parseFloat(this.obj.style.left.slice(0, -2)); },
-        set: function (x) { this.obj.style.left = "".concat(x, "px"); },
+        set: function (x) {
+            this.prev_x = this.x;
+            this.obj.style.left = "".concat(x, "px");
+        },
         enumerable: false,
         configurable: true
     });
     Object.defineProperty(Object2D.prototype, "y", {
         get: function () { return parseFloat(this.obj.style.top.slice(0, -2)); },
-        set: function (y) { this.obj.style.top = "".concat(y, "px"); },
+        set: function (y) {
+            this.prev_y = this.y;
+            this.obj.style.top = "".concat(y, "px");
+        },
         enumerable: false,
         configurable: true
     });
+    Object2D.prototype.doPhysics = function () {
+    };
     Object2D.prototype.rigidbody = function () {
         this.velX -= friction / this.mass * this.velX * deltaTime;
         this.velY += (g - friction / this.mass * this.velY) * deltaTime;
+        if (Math.abs(this.velX) < 0.05)
+            this.velX = 0;
         this.x += this.velX * deltaTime;
         this.y += this.velY * deltaTime;
     };
-    Object2D.prototype.collider = function () {
+    Object2D.prototype.collider = function (entity) {
+        var colided = false;
+        if (this.x < entity.x + entity.size_x &&
+            this.x + this.size_x > entity.x &&
+            this.y < entity.y + entity.size_y &&
+            this.y + this.size_y > entity.y) {
+            // this.obj.style.background = 'blue';
+            if (this.x + this.size_x >= entity.x && this.x + this.size_x <= entity.x + entity.size_x) {
+                if (this.prev_x + this.size_x <= entity.x) {
+                    colided = true;
+                    this.velX = -(this.bounce * Math.abs(this.velX) + (1 - entity.bounce) * Math.abs(entity.velX));
+                    entity.velX = (entity.bounce * Math.abs(entity.velX) + (1 - this.bounce) * Math.abs(this.velX));
+                }
+            }
+            else if (this.x <= entity.x + entity.size_x && this.x + this.size_x >= entity.x + entity.size_x) {
+                if (this.prev_x >= entity.x + entity.size_x) {
+                    colided = true;
+                    this.velX = (this.bounce * Math.abs(this.velX) + (1 - entity.bounce) * Math.abs(entity.velX));
+                    entity.velX = -(entity.bounce * Math.abs(entity.velX) + (1 - this.bounce) * Math.abs(this.velX));
+                }
+            }
+            if (this.y + this.size_y >= entity.y && this.y + this.size_y <= entity.y + entity.size_y) {
+                if (this.prev_y + this.size_y <= entity.y) {
+                    colided = true;
+                    this.y = entity.y - this.size_y + 1;
+                    this.velY = -Math.abs(this.velY);
+                    entity.velY = Math.abs(entity.velY);
+                }
+            }
+            else if (this.y <= entity.y + entity.size_y && this.y + this.size_y >= entity.y + entity.size_y) {
+                if (this.prev_y >= entity.y + entity.size_y) {
+                    colided = true;
+                    entity.y = this.y - entity.size_y + 1;
+                    this.velY = Math.abs(this.velY);
+                    entity.velY = -Math.abs(entity.velY);
+                }
+            }
+            if (colided == false) {
+                if (this.y < entity.y)
+                    this.y = entity.y - this.size_y;
+                if (this.y > entity.y)
+                    entity.y = this.y - entity.size_y;
+            }
+        }
+        else {
+            this.obj.style.background = '#ddaa10';
+        }
     };
     return Object2D;
 }());
@@ -82,7 +146,6 @@ var Box = /** @class */ (function (_super) {
     }
     return Box;
 }(Object2D));
-var isDown = false;
 var Square = /** @class */ (function (_super) {
     __extends(Square, _super);
     function Square(x, y, size, color) {
@@ -92,89 +155,99 @@ var Square = /** @class */ (function (_super) {
         _this.obj.style.background = color;
         _this.prev_x;
         _this.prev_y;
-        _this.obj.onmousedown = sqrClickHandler;
+        _this.dragging = 0;
+        _this.obj.onmousedown = _this.onClickHandler.bind(_this);
+        _this.obj.ontouchstart = _this.onClickHandler.bind(_this);
+        document.addEventListener('mousemove', _this.mouseDrag.bind(_this));
+        _this.obj.onmouseup = function () { return _this.dragging = false; };
+        _this.obj.ontouchend = function () { return _this.dragging = false; };
         return _this;
     }
-    Object.defineProperty(Square.prototype, "x", {
-        get: function () { return parseFloat(this.obj.style.left.slice(0, -2)); },
-        set: function (x) {
-            this.prev_x = this.x;
-            this.obj.style.left = "".concat(x, "px");
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Square.prototype, "y", {
-        get: function () { return parseFloat(this.obj.style.top.slice(0, -2)); },
-        set: function (y) {
-            this.prev_y = this.y;
-            this.obj.style.top = "".concat(y, "px");
-        },
-        enumerable: false,
-        configurable: true
-    });
+    Square.prototype.onClickHandler = function (e) {
+        e.preventDefault();
+        clickX = e.clientX;
+        clickY = e.clientY;
+        this.dragging = true;
+    };
+    Square.prototype.mouseDrag = function (e) {
+        e.preventDefault();
+        if (this.dragging) {
+            this.x += e.clientX - clickX;
+            this.y += e.clientY - clickY;
+            clickX = e.clientX;
+            clickY = e.clientY;
+        }
+    };
+    Square.prototype.touchDrag = function (e) {
+        e.preventDefault();
+        this.x += e.touches[0].clientX - clickX;
+        this.y += e.touches[0].clientY - clickY;
+        clickX = e.touches[0].clientX;
+        clickY = e.touches[0].clientY;
+    };
+    Square.prototype.rigidbody = function () {
+        if (!this.dragging) {
+            _super.prototype.rigidbody.call(this);
+        }
+        else {
+            this.velY = 0.15 * (this.y - this.prev_y) / deltaTime;
+            this.velX = 0.15 * (this.x - this.prev_x) / deltaTime;
+        }
+    };
     return Square;
 }(Object2D));
-var clickX = 0;
-var clickY = 0;
-function sqrClickHandler(e) {
-    clickX = e.clientX;
-    clickY = e.clientY;
-    isDown = !isDown;
-}
 function collide() {
-    if (sq.y + 100 > floor.y) {
-        isDown = false;
-        sq.y = floor.y - 100;
-        if (sq.velY > 0)
-            sq.velY *= -sq.bounce;
-    }
+    // floor
+    // if(sq.y + sq.size_y > floor.y) {
+    //     sq.y = floor.y - 100;
+    //     if(sq.velY > 0)
+    //         sq.velY *= -sq.bounce;
+    // }
+    // if(sq2.y + sq2.size_y > floor.y) {
+    //     sq2.y = floor.y - sq2.size_y;
+    //     if(sq2.velY > 0)
+    //         sq2.velY *= -sq2.bounce;
+    // }
+    //walls
     if (sq.x <= 0) {
-        isDown = false;
         if (sq.velX < 0)
-            sq.velX *= -sq.bounce;
+            sq.dragging = false;
+        sq.velX *= -sq.bounce;
     }
     else if (sq.x >= window.innerWidth - sq.size_x) {
-        isDown = false;
         if (sq.velX > 0) {
+            sq.dragging = false;
             sq.velX *= -sq.bounce;
+        }
+    }
+    if (sq2.x <= 0) {
+        sq2.dragging = false;
+        if (sq2.velX < 0)
+            sq2.velX *= -sq2.bounce;
+    }
+    else if (sq2.x >= window.innerWidth - sq2.size_x) {
+        sq2.dragging = false;
+        if (sq2.velX > 0) {
+            sq2.velX *= -sq2.bounce;
         }
     }
 }
 function start() {
     sq = new Square(100, 100, 100, '#ddaa10');
-    floor = new Box(0, window.innerHeight - 200, screen.width, 200);
+    sq2 = new Square(400, 100, 50, '#ddaa10');
+    floor = new Box(0, window.innerHeight - 200, window.innerWidth, 200);
     lastUpdate = Date.now();
     setInterval(tick, 0);
 }
 function update() {
-    console.log(deltaTime);
-    if (!isDown) {
-        sq.rigidbody();
-    }
-    else {
-        sq.velY = 0.15 * (sq.y - sq.prev_y) / deltaTime;
-        sq.velX = 0.15 * (sq.x - sq.prev_x) / deltaTime;
-    }
+    // console.log(deltaTime);
     collide();
+    sq.collider(floor);
+    sq2.collider(floor);
+    sq.collider(sq2);
+    sq.rigidbody();
+    sq2.rigidbody();
     floor.obj.innerText = "VelX = ".concat(sq.velX, "\n");
     floor.obj.innerText += "VelY = ".concat(sq.velY, "\n");
 }
-// document.addEventListener('click', () => isDown = !isDown);
-document.addEventListener('mousemove', function (e) {
-    if (isDown) {
-        sq.x += e.clientX - clickX;
-        sq.y += e.clientY - clickY;
-        clickX = e.clientX;
-        clickY = e.clientY;
-    }
-});
-document.addEventListener('touchmove', function (e) {
-    if (isDown) {
-        sq.x += e.touches[0].clientX - clickX;
-        sq.y += e.touches[0].clientY - clickY;
-        clickX = e.touches[0].clientX;
-        clickY = e.touches[0].clientY;
-    }
-});
 start();
--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 const friction = 2;
-const g = 0.5;
+const g = 1;
+const objects = [];
+let clickX = 0;
+let clickY = 0;
+let beingDragged = null;
+
 let sq;
+let sq2;
 let floor;
 let lastUpdate;
 let deltaTime = 0;
@@ -17,6 +23,10 @@ class Object2D {
     mass: number
     velX: number
     velY: number
+    prev_x: number;
+    prev_y: number;
+    bounce: number
+    components: Array<Function>
     constructor(x, y, size_x, size_y) {
         this.obj = document.createElement('div');
         this.obj.style.position = 'absolute'
@@ -26,28 +36,87 @@ class Object2D {
 
         this.x = x;
         this.y = y;
+        this.prev_x = this.x;
+        this.prev_y = this.y;
         this.velX = 0;
         this.velY = 0;
         this.mass = 100;
-        this.bounce = 0.8;
+        this.bounce = 0.7;
+        this.components = [];
     }
 
     get size_x() { return parseFloat(this.obj.style.width.slice(0, -2)) }
     get size_y() { return parseFloat(this.obj.style.height.slice(0, -2)) }
     get x() { return parseFloat(this.obj.style.left.slice(0, -2)) }
-    set x(x) { this.obj.style.left = `${x}px`; }
+    set x(x) {
+        this.prev_x = this.x;
+        this.obj.style.left = `${x}px`; 
+    }
     get y() { return parseFloat(this.obj.style.top.slice(0, -2)) }
-    set y(y) {this.obj.style.top = `${y}px`;}
+    set y(y) {
+        this.prev_y = this.y;
+        this.obj.style.top = `${y}px`;
+    }
     
+    doPhysics() {
+        
+    }
+
     rigidbody() {
         this.velX -= friction / this.mass * this.velX * deltaTime;
         this.velY += (g - friction / this.mass * this.velY) * deltaTime;
+        if (Math.abs(this.velX) < 0.05) this.velX = 0;
         this.x += this.velX * deltaTime;
         this.y += this.velY * deltaTime;
     }
 
-    collider() {
+    collider(entity: Object2D) {
+        let colided = false;
+        if (this.x < entity.x + entity.size_x &&
+            this.x + this.size_x > entity.x &&
+            this.y < entity.y + entity.size_y &&
+            this.y + this.size_y > entity.y) {
+            // this.obj.style.background = 'blue';
+            if(this.x + this.size_x >= entity.x && this.x + this.size_x <= entity.x + entity.size_x) {
+                if(this.prev_x + this.size_x <= entity.x) {
+                    colided = true;
+                    this.velX = - (this.bounce * Math.abs(this.velX) + (1 - entity.bounce) * Math.abs(entity.velX));
+                    entity.velX = (entity.bounce * Math.abs(entity.velX) + (1 - this.bounce) * Math.abs(this.velX));
+                }
+            }
+            else if(this.x <= entity.x + entity.size_x && this.x + this.size_x >= entity.x + entity.size_x) {
+                if(this.prev_x >= entity.x + entity.size_x) {
+                    colided = true;
+                    this.velX = (this.bounce * Math.abs(this.velX) + (1 - entity.bounce) * Math.abs(entity.velX));
+                    entity.velX = - (entity.bounce * Math.abs(entity.velX) + (1 - this.bounce) * Math.abs(this.velX));
+                }
+            }
+            if(this.y + this.size_y >= entity.y && this.y + this.size_y <= entity.y + entity.size_y) {
+                if(this.prev_y + this.size_y <= entity.y) {
+                    colided = true;
+                    this.y = entity.y - this.size_y + 1;
+                    this.velY = - Math.abs(this.velY);
+                    entity.velY = Math.abs(entity.velY);
+                }
+            }
+            else if(this.y <= entity.y + entity.size_y && this.y + this.size_y >= entity.y + entity.size_y) {
+                if(this.prev_y >= entity.y + entity.size_y) {
+                    colided = true;
+                    entity.y = this.y - entity.size_y + 1;
+                    this.velY = Math.abs(this.velY);
+                    entity.velY = - Math.abs(entity.velY);
+                }
+            }
+            if(colided == false) {
+                if(this.y < entity.y)
+                    this.y = entity.y - this.size_y;
+                if(this.y > entity.y)
+                    entity.y = this.y - entity.size_y;
+            }
 
+        }else {
+            this.obj.style.background = '#ddaa10';
+        }
     }
 }
 
@@ -58,54 +127,91 @@ class Box extends Object2D{
         this.obj.style.background = color;
     }
 }
-let isDown = false;
+
 class Square extends Object2D{
-    prev_x: Number;
-    prev_y: Number;
+    dragging;
     constructor(x, y, size, color = '#ffffff') {
         super(x, y, size, size);
         this.obj.classList.add('square', 'box-collider', 'rigidbody');
         this.obj.style.background = color;
         this.prev_x;
         this.prev_y;
-        this.obj.onmousedown = sqrClickHandler;
+        this.dragging = 0;
+        this.obj.onmousedown = this.onClickHandler.bind(this);
+        this.obj.ontouchstart = this.onClickHandler.bind(this);
+        document.addEventListener('mousemove', this.mouseDrag.bind(this));
+        this.obj.onmouseup = () => this.dragging = false;
+        this.obj.ontouchend = () => this.dragging = false;
     }
 
-    get x() { return parseFloat(this.obj.style.left.slice(0, -2)) }
-    set x(x) { 
-        this.prev_x = this.x;
-        this.obj.style.left = `${x}px`; 
-    }
-    get y() { return parseFloat(this.obj.style.top.slice(0, -2)) }
-    set y(y) {
-        this.prev_y = this.y;
-        this.obj.style.top = `${y}px`;
+    onClickHandler(e) {
+        e.preventDefault();
+        clickX = e.clientX;
+        clickY = e.clientY;
+        this.dragging = true;
     }
 
-}
-let clickX = 0;
-let clickY = 0;
-function sqrClickHandler(e: MouseEvent) {
-    clickX = e.clientX;
-    clickY = e.clientY;
-    isDown = !isDown;
+    mouseDrag(e) {
+        e.preventDefault();
+        if(this.dragging) {
+            this.x += e.clientX - clickX;
+            this.y += e.clientY - clickY;
+            clickX = e.clientX;
+            clickY = e.clientY;
+        }
+    }
+
+    touchDrag(e) {
+        e.preventDefault();
+        this.x += e.touches[0].clientX - clickX;
+        this.y += e.touches[0].clientY - clickY;
+        clickX = e.touches[0].clientX;
+        clickY = e.touches[0].clientY;
+    }
+
+    rigidbody() {
+        if(!this.dragging) {
+            super.rigidbody();
+        }else {
+            this.velY = 0.15 * (this.y - this.prev_y) / deltaTime;
+            this.velX = 0.15 * (this.x - this.prev_x) / deltaTime;
+        }
+    }
+
 }
 
 function collide() {
-    if(sq.y + 100 > floor.y) {
-        isDown = false;
-        sq.y = floor.y - 100;
-        if(sq.velY > 0)
-            sq.velY *= -sq.bounce;
-    }
+    // floor
+    // if(sq.y + sq.size_y > floor.y) {
+    //     sq.y = floor.y - 100;
+    //     if(sq.velY > 0)
+    //         sq.velY *= -sq.bounce;
+    // }
+    // if(sq2.y + sq2.size_y > floor.y) {
+    //     sq2.y = floor.y - sq2.size_y;
+    //     if(sq2.velY > 0)
+    //         sq2.velY *= -sq2.bounce;
+    // }
+
+    //walls
     if(sq.x <= 0) {
-        isDown = false;
         if(sq.velX < 0)
+            sq.dragging = false;
             sq.velX *= -sq.bounce;
     }else if(sq.x >= window.innerWidth - sq.size_x) {
-        isDown = false;
         if(sq.velX > 0) {
+            sq.dragging = false;
             sq.velX *= -sq.bounce;
+        }
+    }
+    if(sq2.x <= 0) {
+        sq2.dragging = false;
+        if(sq2.velX < 0)
+            sq2.velX *= -sq2.bounce;
+    }else if(sq2.x >= window.innerWidth - sq2.size_x) {
+        sq2.dragging = false;
+        if(sq2.velX > 0) {
+            sq2.velX *= -sq2.bounce;
         }
     }
 }
@@ -113,40 +219,23 @@ function collide() {
 
 function start() {
     sq = new Square(100, 100, 100, '#ddaa10');
-    floor = new Box(0, window.innerHeight - 200, screen.width, 200);
+    sq2 = new Square(400, 100, 50, '#ddaa10');
+    floor = new Box(0, window.innerHeight - 200, window.innerWidth, 200);
     lastUpdate = Date.now();
     setInterval(tick, 0);
 }
 
 function update() {
-    console.log(deltaTime);
-    if(!isDown) {
-        sq.rigidbody();
-    }else {
-        sq.velY = 0.15 * (sq.y - sq.prev_y) / deltaTime;
-        sq.velX = 0.15 * (sq.x - sq.prev_x) / deltaTime;
-    }
+    // console.log(deltaTime);
     collide();
+    sq.collider(floor);
+    sq2.collider(floor);
+    sq.collider(sq2);
+    
+    sq.rigidbody();
+    sq2.rigidbody();
     (floor.obj as HTMLElement).innerText = `VelX = ${sq.velX}\n`;
     (floor.obj as HTMLElement).innerText += `VelY = ${sq.velY}\n`;
 }
-
-// document.addEventListener('click', () => isDown = !isDown);
-document.addEventListener('mousemove', (e) => {
-    if(isDown) {
-        sq.x += e.clientX - clickX;
-        sq.y += e.clientY - clickY;
-        clickX = e.clientX;
-        clickY = e.clientY;
-    }
-});
-document.addEventListener('touchmove', (e) => {
-    if(isDown) {
-        sq.x += e.touches[0].clientX - clickX;
-        sq.y += e.touches[0].clientY - clickY;
-        clickX = e.touches[0].clientX;
-        clickY = e.touches[0].clientY;
-    }
-});
 
 start();
